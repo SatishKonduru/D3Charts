feea101d6b4b1b45969f5225c40d1f8977017bdd
satish <satishkonduru0225@gmail.com>
2024-02-05 12:02:38 +0530

    margin to column chart

--- D3/src/app/components/charts/chart1/chart1.component.ts
+++ D3/src/app/components/charts/chart1/chart1.component.ts
@@ -6,16 +6,36 @@ import { Component, ElementRef, OnInit } from '@angular/core';
   styleUrl: './chart1.component.css'
 })
 export class Chart1Component implements OnInit{
-data = [120,250,350,650,50]
+data = [55, 75, 50, 120,250,350,55, 200]
 rectWidth: any;
 max : number;
-dimensions : DOMRect;''
+dimensions : DOMRect;
+outerPadding = 20;
+padding = 0;
+bandWidth = 0;
+bandWidthCoef = 0.8 ; // = 80%
+
+left= 10; right=20; bottom=30; top=15;
+// margin = {
+//   left: 10,
+//   right: 20,
+//   bottom: 30,
+//   top: 15
+// }
+
+innerWidth : number;
+innerHeight: number;
 
 constructor(private _eleRef: ElementRef){}
 ngOnInit(): void {
   const svg = this._eleRef.nativeElement.getElementsByTagName('svg')[0]
   this.dimensions = svg.getBoundingClientRect();
-  this.rectWidth = this.dimensions.width / this.data.length;
+  this.innerWidth = this.dimensions.width  - this.left - this.right;
+  this.innerHeight = this.dimensions.height - this.top - this.bottom;
+
+  this.rectWidth = (this.innerWidth - 2 * this.outerPadding) / this.data.length;
+  this.bandWidth  = this.bandWidthCoef * this.rectWidth;
+  this.padding = (1 - this.bandWidthCoef) * this.rectWidth;
   this.max = 1.3 * Math.max(...this.data)
 }
 }
